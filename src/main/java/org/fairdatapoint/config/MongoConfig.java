e6aff4e2b87c81a78fc4809e63600fe8ca456c92
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-01-10 13:54:15 +0100

    fix mongo config for acls

--- src/main/java/org/fairdatapoint/config/MongoConfig.java
+++ src/main/java/org/fairdatapoint/config/MongoConfig.java
@@ -43,7 +43,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
         basePackages = {
             "org.fairdatapoint",
             "org.fairdatateam.rdf.migration",
-            "org.springframework.security.acls"
+            "org.fairdatateam.security.acls"
         }
 )
 public class MongoConfig {
